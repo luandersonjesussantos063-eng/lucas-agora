22803e0a75ffa6cd088480cdb72cf7c6d1bded43
luandersonjesussantos063-eng <luandersonjesussantos063@gmail.com>
2026-09-20 13:21:17 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE='lucas-agora-v2-shell';
-const ASSETS=['./','./index.html','./manifest.json','./config.js'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
-});
