28a3dbaa330a8878fedf3cc6137f83c1156faeb0
luandersonjesussantos063-eng <luandersonjesussantos063@gmail.com>
2026-09-20 15:56:58 -0400

    Update service worker cache and fetch handling

--- sw.js
+++ sw.js
@@ -1,31 +1,64 @@
-const CACHE='lucas-agora-v6-shell';
-const ASSETS=['./','./index.html','./manifest.json'];
+const CACHE = 'lucas-agora-v7-shell';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
 self.addEventListener('install', event => {
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+
+  event.waitUntil(
+    caches
+      .open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    caches
+      .keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+        )
+      )
       .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
   const url = new URL(event.request.url);
-  if (url.origin !== self.location.origin) return;
+
+  // Não interfere nas chamadas externas do Supabase
+  if (url.origin !== self.location.origin) {
+    return;
+  }
 
   event.respondWith(
     fetch(event.request)
       .then(response => {
         const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+
+        caches
+          .open(CACHE)
+          .then(cache => cache.put(event.request, copy))
+          .catch(() => {});
+
         return response;
       })
-      .catch(() => caches.match(event.request).then(r => r || caches.match('./index.html')))
+      .catch(() =>
+        caches
+          .match(event.request)
+          .then(response => response || caches.match('./index.html'))
+      )
   );
 });
