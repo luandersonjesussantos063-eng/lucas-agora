5375294a17f3390b9220d857d8fb10d990f1cba1
luandersonjesussantos063-eng <luandersonjesussantos063@gmail.com>
2026-09-21 11:27:54 -0400

    Atualizar interface e destacar parceria Nova-byte

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='anuncia-lucas-v8-shell';
+const CACHE='anuncia-lucas-v9-shell';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./privacy.html','./terms.html','./support.html','./delete-account.html','./legal.css'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>(key.startsWith('anuncia-lucas-')||key.startsWith('lucas-agora-'))&&key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
